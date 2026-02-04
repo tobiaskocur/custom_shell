18f6f853b27f9eb6fb5acbc8c9c1b98a89d9a77c
Tobias Kocur <skylergamingpro@gmail.com>
2026-02-04 06:28:23 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <string>
 
+void error_message(const std::string &cmd) {
+  std::cerr << cmd << " was not found" << std::endl;
+}
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
@@ -8,4 +12,9 @@ int main() {
 
   // TODO: Uncomment the code below to pass the first stage
   std::cout << "$ ";
+
+  std::string line;
+  std::getline(std::cin, line);
+
+  error_message(line);
 }
