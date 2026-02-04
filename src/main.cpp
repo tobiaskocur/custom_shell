656d9172f50e766204d3e5bdeaf4b5c30d251f70
Tobias Kocur <skylergamingpro@gmail.com>
2026-02-04 06:54:03 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,43 @@
+#include <functional>
 #include <iostream>
 #include <string>
 
+typedef std::function<void()>(*Action);
+typedef void (*cmd_one_param)(std::string param);
 void error_message(const std::string &cmd) {
   std::cerr << cmd << ": command not found" << std::endl;
 }
 
+std::string commands[] = {"exit", "echo"};
+
+void exitd() {
+  exit(0);
+}
+
+void echo() {
+  std::string buffer;
+  std::getline(std::cin, buffer);
+  std::cout << buffer << std::endl;
+}
+
 int main() {
   // Flush after every std::cout / std:cerr
+
+  void (*functions[])() = {exitd, echo};
   while (true) {
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
     std::cout << "$ ";
 
-    std::string line;
-    std::getline(std::cin, line);
+    std::string cmd;
+    std::cin >> cmd;
 
-    if (line == "exit") {
-      exit(0);
+    for (int i = 0; i < commands->length(); i++) {
+      if (cmd == commands[i]) {
+        functions[i]();
+      }
     }
-
-    error_message(line);
+    error_message(cmd);
   }
 }
