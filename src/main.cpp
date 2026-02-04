a700aaa8f99f6cadc406f2307f10fc285a95a475
Tobias Kocur <skylergamingpro@gmail.com>
2026-02-04 08:44:07 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -34,9 +34,9 @@ void type_cmd() {
   std::string path = std::getenv("PATH");
 
   std::string bufferx;
-  while (path.length() <= 0) {
+  while (path.length() > 0) {
     bufferx = path.substr(0, path.find(':'));
-    path = path.substr(path.find(':'), path.length());
+    path = path.substr(path.find(':')+1, path.length());
 
     for ( auto &p : std::filesystem::directory_iterator(bufferx)) {
       std::cout << p;
